edb03402541e6723b4e1b2c0c13c65cb365b4a73
Jacques von Hämsterviel <59958130+JacquesVonHamsterviel@users.noreply.github.com>
2022-02-27 17:30:08 +0800

    KeyGen-TPA

---
+++ TPA/My/MyApplication.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.ApplicationServices;
+
+namespace WindowsApplication1.My
+{
+	// Token: 0x02000002 RID: 2
+	[GeneratedCode("MyTemplate", "8.0.0.0")]
+	[EditorBrowsable(EditorBrowsableState.Never)]
+	internal class MyApplication : WindowsFormsApplicationBase
+	{
+		// Token: 0x06000001 RID: 1 RVA: 0x000027C0 File Offset: 0x00000BC0
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		[DebuggerHidden]
+		[STAThread]
+		internal static void Main(string[] Args)
+		{
+			try
+			{
+				Application.SetCompatibleTextRenderingDefault(WindowsFormsApplicationBase.UseCompatibleTextRendering);
+			}
+			finally
+			{
+			}
+			MyProject.Application.Run(Args);
+		}
+
+		// Token: 0x06000002 RID: 2 RVA: 0x000027F8 File Offset: 0x00000BF8
+		[DebuggerStepThrough]
+		public MyApplication() : base(AuthenticationMode.Windows)
+		{
+			this.IsSingleInstance = false;
+			this.EnableVisualStyles = true;
+			this.SaveMySettingsOnExit = true;
+			this.ShutdownStyle = ShutdownMode.AfterMainFormCloses;
+		}
+
+		// Token: 0x06000003 RID: 3 RVA: 0x00002820 File Offset: 0x00000C20
+		[DebuggerStepThrough]
+		protected override void OnCreateMainForm()
+		{
+			this.MainForm = MyProject.Forms.Form1;
+		}
+	}
+}
